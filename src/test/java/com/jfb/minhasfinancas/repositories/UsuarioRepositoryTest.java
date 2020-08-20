ae7e56b4f1997af6039a9f41c4ce085a0733310c
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-08-20 19:29:00 -0300

    Ativando profiles de test para testes de integração e add o H2.

--- src/test/java/com/jfb/minhasfinancas/repositories/UsuarioRepositoryTest.java
+++ src/test/java/com/jfb/minhasfinancas/repositories/UsuarioRepositoryTest.java
@@ -5,15 +5,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.jfb.minhasfinancas.model.entity.Usuario;
 
-import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
-
 @SpringBootTest
 @RunWith(SpringRunner.class)
-@AutoConfigureEmbeddedDatabase
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 
 	@Autowired
