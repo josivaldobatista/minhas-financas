0718ab38a6d82ce71e23f5a79b9f442b58bbbe6e
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-08-22 20:19:58 -0300

    Método de autenticar usuario.(OK)

--- src/main/java/com/jfb/minhasfinancas/resources/UsuarioResource.java
+++ src/main/java/com/jfb/minhasfinancas/resources/UsuarioResource.java
@@ -1,10 +1,12 @@
 package com.jfb.minhasfinancas.resources;
 
+import com.jfb.minhasfinancas.exceptions.ErroAutenticacao;
 import com.jfb.minhasfinancas.exceptions.RegraNegocioException;
 import com.jfb.minhasfinancas.model.dto.UsuarioDTO;
 import com.jfb.minhasfinancas.model.entity.Usuario;
 import com.jfb.minhasfinancas.services.UsuarioService;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,16 @@ public class UsuarioResource {
     
     @Autowired
     private UsuarioService service;
+
+    @PostMapping("/autenticar")
+    public ResponseEntity<?> autenticar(@RequestBody UsuarioDTO dto) {
+        try {
+            Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
+            return ResponseEntity.ok(usuarioAutenticado);
+        } catch (ErroAutenticacao e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } 
+    }
 
     @PostMapping
     public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
