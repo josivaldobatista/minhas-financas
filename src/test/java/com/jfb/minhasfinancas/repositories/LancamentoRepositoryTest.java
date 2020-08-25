f89375b27c8c29dbc7e0f1c9bc2764ee9988dcff
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-08-25 14:46:33 -0300

    Testes de Integração para Lançamento, Métodos: atualizar e buscarPorId.(OK)

--- src/test/java/com/jfb/minhasfinancas/repositories/LancamentoRepositoryTest.java
+++ src/test/java/com/jfb/minhasfinancas/repositories/LancamentoRepositoryTest.java
@@ -2,12 +2,13 @@ package com.jfb.minhasfinancas.repositories;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Optional;
 
 import com.jfb.minhasfinancas.model.entity.Lancamento;
 import com.jfb.minhasfinancas.model.enums.StatusLancamento;
 import com.jfb.minhasfinancas.model.enums.TipoLancamento;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +31,45 @@ public class LancamentoRepositoryTest {
     @Autowired
     TestEntityManager entityManager;
 
+    @Test
+    public void deveSalvarUmLancamento() {
+        Lancamento obj = criarEPersisteUmLancamento();
+        assertThat(obj.getId()).isNotNull();
+    }
+
+    @Test
+    public void deveDeletarUmLancamento() {
+    	Lancamento obj = criarLancamento();
+    	entityManager.persist(obj); // Salva um lançamento.
+    	obj = entityManager.find(Lancamento.class, obj.getId()); // Busca o lançamento que foi  salvo.
+    	
+    	repository.delete(obj);
+    	Lancamento lancamentoInexistente = entityManager.find(Lancamento.class, obj.getId());
+    	assertThat(lancamentoInexistente).isNull();
+    }
+    
+    @Test
+    public void deveAtualizarUmLancamento() {
+    	Lancamento obj = criarEPersisteUmLancamento();
+    	obj.setAno(2021);
+    	obj.setDescricao("Outra descrição qualquer.");
+    	obj.setStatus(StatusLancamento.CANCELADO);
+    	repository.save(obj);
+    	
+    	Lancamento objAtualizado = entityManager.find(Lancamento.class, obj.getId());
+    	assertThat(objAtualizado.getAno()).isEqualTo(2021);
+    	assertThat(objAtualizado.getDescricao()).isEqualTo("Outra descrição qualquer.");
+    	assertThat(objAtualizado.getStatus()).isEqualTo(StatusLancamento.CANCELADO);
+    }
+    
+    @Test
+    public void deveBuscarUmLancamentoPorId() {
+    	Lancamento obj = criarEPersisteUmLancamento();
+    	Optional<Lancamento> objEncontrado = repository.findById(obj.getId());
+    	assertThat(objEncontrado.isPresent()).isTrue();
+    }
+    
+    // Métodos de apoio
     private Lancamento criarLancamento() {
     	return Lancamento.builder()
     			.ano(2020)
@@ -42,40 +82,9 @@ public class LancamentoRepositoryTest {
     			.build();
     }
     
-    @Test
-    public void deveSalvarUmLancamento() {
-        Lancamento obj = criarLancamento();
-        obj = repository.save(obj);
-        Assertions.assertThat(obj.getId()).isNotNull();
-    }
-
-    
-    @Test
-    public void deveDeletarUmLancamento() {
+    private Lancamento criarEPersisteUmLancamento() {
     	Lancamento obj = criarLancamento();
-    	entityManager.persist(obj); // Salva um lançamento.
-    	obj = entityManager.find(Lancamento.class, obj.getId()); // Busca o lançamento que foi  salvo.
-    	
-    	repository.delete(obj);
-    	Lancamento lancamentoInexistente = entityManager.find(Lancamento.class, obj.getId());
-    	Assertions.assertThat(lancamentoInexistente).isNull();
+    	obj = repository.save(obj);
+    	return obj;
     }
-    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
